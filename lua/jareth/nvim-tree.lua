550357b702ef25256099874a28878a92401bc087
Jareth Gomes <jarethgomes@gmail.com>
2023-01-26 07:28:33 +0000

    Fixed nvim-tree startup errors

--- lua/jareth/nvim-tree.lua
+++ lua/jareth/nvim-tree.lua
@@ -50,10 +50,10 @@ nvim_tree.setup {
     },
     view = {
         width = 30,
-        height = 30,
+        -- height = 30,
         hide_root_folder = false,
         side = "left",
-        auto_resize = true,
+        -- auto_resize = true,
         mappings = {
             custom_only = false,
             list = {
@@ -66,8 +66,10 @@ nvim_tree.setup {
         relativenumber = false,
     },
     actions = {
-        quit_on_open = true,
-        window_picker = { enable = true },
+        open_file = {
+            quit_on_open = true,
+            window_picker = { enable = true },
+        }
     },
     renderer = {
         highlight_git = true,
