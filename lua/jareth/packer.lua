5d6fcba135273ec211d073ac4c04060851de0feb
Jareth Gomes <jarethgomes@gmail.com>
2023-04-21 05:23:25 +0000

    Added trouble.nvim

--- lua/jareth/packer.lua
+++ lua/jareth/packer.lua
@@ -92,6 +92,18 @@ return packer.startup(function(use)
     use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
     use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
 
+    use {
+        "folke/trouble.nvim",
+        requires = "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("trouble").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
     -- Go
     use 'ray-x/go.nvim'
     use 'ray-x/guihua.lua' -- recommanded if need floating window support
