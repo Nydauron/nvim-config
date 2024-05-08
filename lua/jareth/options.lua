b27b1609b475ee2335ce5004f1ecd23c04053d4d
Jareth Gomes <jarethgomes@gmail.com>
2024-05-08 14:50:32 +0000

    Added WSL clipboard setting from nvim manual

--- lua/jareth/options.lua
+++ lua/jareth/options.lua
@@ -58,3 +58,20 @@ vim.cmd [[
     set listchars=eol:↵,tab:>.,trail:~,extends:>,precedes:<
     set list
 ]]
+
+if vim.fn.filereadable("/proc/sys/fs/binfmt_misc/WSLInterop") then
+    vim.cmd [[
+        let g:clipboard = {
+        \   'name': 'WslClipboard',
+        \   'copy': {
+        \      '+': 'clip.exe',
+        \      '*': 'clip.exe',
+        \    },
+        \   'paste': {
+        \      '+': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        \      '*': 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        \   },
+        \   'cache_enabled': 0,
+        \ }
+    ]]
+end
